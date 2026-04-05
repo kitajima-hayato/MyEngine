221eefa6fccab57cc15dc4bd6b342c34446d9e27
kitajima-hayato <k023g0057@g.neec.ac.jp>
2026-04-05 15:53:33 +0900

    SpriteCommonの解放忘れを修正

--- engine/InsideScene/Framework.cpp
+++ engine/InsideScene/Framework.cpp
@@ -20,7 +20,7 @@ void Framework::Initialize()
 	srvManager = make_unique<SrvManager>();
 	srvManager->Initialize(dxCommon.get());
 	// スプライトコモンの初期化
-	SpriteCommon::GetInstance().Initialize(dxCommon.get());
+	SpriteCommon::GetInstance()->Initialize(dxCommon.get());
 	// テクスチャマネージャーの初期化
 	TextureManager::GetInstance()->Initialize(dxCommon.get(), srvManager.get());
 
@@ -92,14 +92,20 @@ void Framework::Finalize()
 
 	SceneManager::GetInstance()->Finalize();
 	SceneManager::DestroyInstance();
-	Audio::GetInstance().DeleteInstance();
-	Object3DCommon::GetInstance()->DeleteInstance();
-	TextureManager::GetInstance()->DeleteInstance();
-	SpriteCommon::GetInstance().DeleteInstance();
-	
+
 	ParticleManager::GetInstance()->DeleteInstance();
+
+	Object3DCommon::GetInstance()->DeleteInstance();
+	SpriteCommon::DeleteInstance();
+
+	TextureManager::GetInstance()->DeleteInstance();
 	ModelManager::GetInstance().Finalize();
+
+	Audio::GetInstance().DeleteInstance();
 	Input::GetInstance()->DeleteInstance();
+	
+	
+	
 	mainCamera_ = nullptr;
 	winAPI->Finalize();
 
